a07b0fe591a0a63ed17bff81f9871213604e8b29
berlysia <berlysia@gmail.com>
2024-12-27 18:50:21 +0900

    fix resolver

--- app/ManifestResolver.tsx
+++ app/ManifestResolver.tsx
@@ -12,11 +12,11 @@ for (const manifestFile of Object.values(MANIFEST)) {
 }
 
 const BASE = import.meta.env.VITE_BASE_PATH ?? "/";
-console.log("BASE", BASE);
 
 export default function ManifestResolver({
   src,
   async,
+  prod,
   manifest,
   nonce,
   type,
@@ -28,13 +28,21 @@ export default function ManifestResolver({
   nonce?: string;
   type: "script" | "style";
 }) {
-  const givenManifest = manifest ?? defaultManifest;
-  const resolvedSrc =
-    givenManifest && givenManifest[src.replace(/^\//, "")]?.file;
-  const scriptSrc = resolvedSrc ? `${BASE}${resolvedSrc}` : src;
+  if (prod ?? import.meta.env.PROD) {
+    const givenManifest = manifest ?? defaultManifest;
+    const resolvedSrc =
+      givenManifest && givenManifest[src.replace(/^\//, "")]?.file;
+    const scriptSrc = resolvedSrc ? `${BASE}${resolvedSrc}` : src;
 
-  if (type === "script") {
-    return <script src={scriptSrc} type="module" async={async} nonce={nonce} />;
+    if (type === "script") {
+      return (
+        <script src={scriptSrc} type="module" async={async} nonce={nonce} />
+      );
+    }
+    return <link rel="stylesheet" href={scriptSrc} />;
   }
-  return <link rel="stylesheet" href={scriptSrc} />;
+  if (type === "script") {
+    return <script src={src} type="module" async={async} nonce={nonce} />;
+  }
+  return <link rel="stylesheet" href={src} />;
 }
